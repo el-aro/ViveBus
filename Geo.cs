9eef4fcc7c35b447866e5c624826a91503ce7319
eaplmx <eapl.mx@gmail.com>
2013-08-02 01:19:42 -0600

    Initial commit
    My first commit to GitHub !

---
+++ Geo.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViveBus
+{
+	class Geo
+	{
+		public struct Point
+		{
+			public double latitude;
+			public double longitude;
+
+			public Point(double _latitude, double _longitude)
+			{
+				latitude = _latitude;
+				longitude = _longitude;
+			}
+		}
+
+		public struct Place
+		{
+			public int id;
+			public string name;
+			public Point coordinates;
+		}
+
+		public struct Venue
+		{
+			public string id;
+			public string name;
+			public string searchable_name;
+			public Point coordinates;
+		}
+
+		public struct Node
+		{
+			public int id;
+			public int destination;
+			public int weight;
+		}
+
+		public static double Distance(Point start, Point end)
+		{
+			double theta = start.longitude - end.longitude;
+			double dist = Math.Sin(deg2rad(start.latitude)) * Math.Sin(deg2rad(end.latitude))
+				+ Math.Cos(deg2rad(start.latitude)) * Math.Cos(deg2rad(end.latitude)) * Math.Cos(deg2rad(theta));
+			dist = Math.Acos(dist);
+			dist = rad2deg(dist);
+			dist = dist * 111.1896;
+			
+			return (dist);
+		}
+
+		private static double deg2rad(double deg)
+		{
+			return (deg * Math.PI / 180.0);
+		}
+
+		private static double rad2deg(double rad)
+		{
+			return (rad / Math.PI * 180.0);
+		}
+
+		public static string getDistanceString(double distance)
+		{
+			if (distance < 1)
+			{
+				// Convert to meters
+				return String.Format("{0:0}", distance * 1000) + " metros";
+			}
+			else
+			{
+				return String.Format("{0:#.##}", distance) + " km";
+			}
+		}
+
+		public static KeyValuePair<int, double> getLowestDistanceId(Point origin, List<Place> places)
+		{
+			KeyValuePair<int, double> lowest_distance = new KeyValuePair<int, double>(0, double.MaxValue);
+			Dictionary<int, double> distances = new Dictionary<int, double>();
+			foreach (Geo.Place place in places)
+			{
+				double current_distance = Geo.Distance(origin, place.coordinates);
+				distances.Add(place.id, current_distance);
+
+				if (current_distance < lowest_distance.Value)
+				{
+					lowest_distance = new KeyValuePair<int, double>(place.id, current_distance);
+				}
+			}
+
+			return lowest_distance;
+		}
+
+		public static List<Place> loadPlacesFromCSV(string file_path)
+		{
+			List<Place> places = new List<Place>();
+			
+			string[] csv_lines = System.IO.File.ReadAllLines(file_path);
+			int csv_lines_num = csv_lines.Length;
+			if (csv_lines_num > 1)
+			{
+				for (int current_line = 1; current_line < csv_lines_num; ++current_line)
+				{
+					string[] parts = csv_lines[current_line].Split(',');
+
+					Geo.Place place = new Geo.Place();
+					int.TryParse(parts[0], out place.id);
+					place.name = parts[1];
+
+					double latitude;
+					double longitude;
+					double.TryParse(parts[2], out longitude);
+					double.TryParse(parts[3], out latitude);
+					place.coordinates = new Geo.Point(latitude, longitude);
+
+					places.Add(place);
+				}
+			}
+
+			return places;
+		}
+
+		public static List<Node> loadNodesFromCSV(string file_path)
+		{
+			List<Node> nodes = new List<Node>();
+
+			string[] csv_lines = System.IO.File.ReadAllLines(file_path);
+			int csv_lines_num = csv_lines.Length;
+			if (csv_lines_num > 1)
+			{
+				for (int current_line = 1; current_line < csv_lines_num; ++current_line)
+				{
+					string[] parts = csv_lines[current_line].Split(',');
+
+					Geo.Node node = new Geo.Node();
+					int.TryParse(parts[0], out node.id);
+					int.TryParse(parts[2], out node.destination);
+					int.TryParse(parts[3], out node.weight);
+
+					nodes.Add(node);
+				}
+			}
+
+			return nodes;
+		}
+
+		public static List<Venue> loadVenuesFromCSV(string file_path)
+		{
+			List<Venue> venues = new List<Venue>();
+
+			string[] csv_lines = System.IO.File.ReadAllLines(file_path);
+			int csv_lines_num = csv_lines.Length;
+			if (csv_lines_num > 1)
+			{
+				for (int current_line = 1; current_line < csv_lines_num; ++current_line)
+				{
+					string[] parts = csv_lines[current_line].Split('\t');
+
+					Geo.Venue venue = new Geo.Venue();
+
+					venue.id = parts[0];
+					venue.name = parts[1];
+					venue.searchable_name = parts[2];
+
+					double latitude;
+					double longitude;
+					double.TryParse(parts[4], out longitude);
+					double.TryParse(parts[3], out latitude);
+					venue.coordinates = new Geo.Point(latitude, longitude);
+
+					venues.Add(venue);
+				}
+			}
+
+			return venues;
+		}
+
+		public static List<Node> getWeightFromDistances(List<Place> places, List<Node> nodes)
+		{
+			List<Node> nodes_with_distances = new List<Node>();
+			foreach (Node current_node in nodes)
+			{
+				Node node_with_distance = new Node();
+				int start = current_node.id;
+				int end = current_node.destination;
+
+				double weight = Distance(getPlaceFromId(start, places), getPlaceFromId(end, places));
+
+				node_with_distance = current_node;
+				node_with_distance.weight = (int)weight;
+
+				nodes_with_distances.Add(node_with_distance);
+			}
+
+			return nodes_with_distances;
+		}
+
+		public static void saveNodesToCSV(List<Node> nodes, string file_path)
+		{
+			List<string> lines = new List<string>();
+
+			foreach (Node current_node in nodes)
+			{
+				string line = current_node.id + "," + current_node.destination + "," + current_node.weight;
+				lines.Add(line);
+			}
+
+			System.IO.File.WriteAllLines(file_path, lines.ToArray());
+		}
+
+		public static Point getPlaceFromId(int id, List<Place> places)
+		{
+			// TO DO: Use search instead
+
+			foreach (Place current_place in places)
+			{
+				if (current_place.id == id)
+				{
+					return current_place.coordinates;
+				}
+			}
+
+			return new Point();
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,609 @@
+﻿/**
+ * GeekNights CUU - App for ViveBus
+ * Module by @eaplmx
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Dijkstras_Algorithm;
+
+namespace ViveBus
+{
+	/// <summary>
+	/// Sample code to find public transport routes between a start and end points
+	/// Ask for two places, and get the bus stations to take between them
+	/// </summary>
+	class Program
+	{
+		static void Main(string[] args)
+		{	
+			// Load all our data from CSV files
+			string executable_path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+			string places_file_path = executable_path + System.IO.Path.DirectorySeparatorChar + "stations.csv";
+			List<Geo.Place> places = Geo.loadPlacesFromCSV(places_file_path);
+
+			string nodes_file_path = executable_path + System.IO.Path.DirectorySeparatorChar + "nodes.csv";
+			List<Geo.Node> nodes = Geo.loadNodesFromCSV(nodes_file_path);
+
+			string venues_file_path = executable_path + System.IO.Path.DirectorySeparatorChar + "venues.csv";
+			List<Geo.Venue> venues = Geo.loadVenuesFromCSV(venues_file_path);
+
+			// Save the distances between each place, to set the weight for the edges
+			// Only run this once to generate the table
+			/*
+			file_path = executable_paht + System.IO.Path.DirectorySeparatorChar + "nodes_distances.csv";
+			Geo.saveNodesToCSV(Geo.getWeightFromDistances(places, nodes), file_path);
+			 * */
+
+			double distance;
+
+			// DEBUG: Origin and destination points for tests
+			//Geo.Point origin = new Geo.Point(28.664536, -106.109754);
+			//Geo.Point destination = new Geo.Point(28.634536, -106.102754);
+
+			string station_name = string.Empty;
+
+			Console.WriteLine("¿Dónde estás?");
+
+			bool found_venues = false;
+			List<Geo.Venue> origin_venues = new List<Geo.Venue>();
+			
+
+			while (found_venues == false)
+			{
+				string origin_venue = Console.ReadLine();
+
+				Console.WriteLine("");
+				Console.WriteLine("Encontré los siguientes lugares, selecciona uno:");
+
+				// Get input from the user, which is your selection?
+				origin_venues = searchVenues(origin_venue, venues);
+
+				if (origin_venues.Count == 0)
+				{
+					Console.WriteLine("No encontré lugares con ese nombre, intenta otro");
+				}
+				else
+				{
+					found_venues = true;
+				}
+			}
+
+			int current_venue_num = 1; // The list starts with 1
+			foreach (Geo.Venue current_venue in origin_venues)
+			{
+				Console.WriteLine(current_venue_num + " - " + current_venue.name);
+				++current_venue_num;
+			}
+
+			Geo.Venue origin_venue_selected = new Geo.Venue();
+			bool invalid_selection = true;
+			while (invalid_selection)
+			{
+				string selected_origin_string = Console.ReadLine();
+				int selected_origin;
+				int.TryParse(selected_origin_string, out selected_origin);
+				if (selected_origin > 0 && (selected_origin - 1) < origin_venues.Count)
+				{
+					origin_venue_selected = origin_venues[selected_origin - 1];
+					invalid_selection = false;
+				}
+				else
+				{
+					Console.SetCursorPosition(0, Console.CursorTop - 1);
+				}
+			}
+
+			Geo.Point origin = origin_venue_selected.coordinates;
+
+			Console.WriteLine("");
+			Console.WriteLine("¿A dónde quieres llegar?");
+
+			// DRY - F***!!!
+			string destination_venue = Console.ReadLine();
+
+			Console.WriteLine("");
+			Console.WriteLine("Encontré los siguientes lugares, selecciona uno:");
+
+			current_venue_num = 1;
+			origin_venues = searchVenues(destination_venue, venues);
+			foreach (Geo.Venue current_venue in origin_venues)
+			{
+				Console.WriteLine(current_venue_num + " - " + current_venue.name);
+				++current_venue_num;
+			}
+
+			Geo.Venue destination_venue_selected = new Geo.Venue();
+			invalid_selection = true;
+			while (invalid_selection)
+			{
+				string selected_origin_string = Console.ReadLine();
+				int selected_origin;
+				int.TryParse(selected_origin_string, out selected_origin);
+				if (selected_origin > 0 && (selected_origin - 1) < origin_venues.Count)
+				{
+					destination_venue_selected = origin_venues[selected_origin - 1];
+					invalid_selection = false;
+				}
+				else
+				{
+					Console.SetCursorPosition(0, Console.CursorTop - 1);
+				}
+			}
+
+			Geo.Point destination = destination_venue_selected.coordinates;
+
+			Console.WriteLine("---------------------");
+
+			// Get the distances to all the stations
+			KeyValuePair<int, double> lowest_distance = Geo.getLowestDistanceId(origin, places);
+			int origin_id = lowest_distance.Key;
+
+			foreach (Geo.Place place in places)
+			{
+				if (place.id == lowest_distance.Key)
+				{
+					station_name = place.name;
+				}
+			}
+
+			Console.WriteLine("Ve en alfombra voladora ");
+			distance = lowest_distance.Value;
+			Console.WriteLine(Geo.getDistanceString(distance));
+			Console.WriteLine("hasta");
+			Console.WriteLine(station_name);
+
+			station_name = string.Empty;
+			lowest_distance = Geo.getLowestDistanceId(destination, places);
+
+			int destination_id = lowest_distance.Key;
+
+			foreach (Geo.Place place in places)
+			{
+				if (place.id == lowest_distance.Key)
+				{
+					station_name = place.name;
+				}
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("baja en");
+			Console.WriteLine(station_name);
+			Console.WriteLine();
+			Console.WriteLine("y teletranspórtate ");
+
+			distance = lowest_distance.Value;
+			Console.WriteLine(Geo.getDistanceString(distance));
+
+			Console.WriteLine("hasta tu destino ");
+
+			Graph graph = new Graph();
+			Dictionary<int, Vector2D> resultIndexVertexMapping = new Dictionary<int, Vector2D>();
+
+			foreach (Geo.Place current_place in places)
+			{
+				graph.AddVertex(new Vector2D(0, 0, false, current_place.id));
+			}
+
+			List<Vector2D> graph_nodes = graph.AllNodes;
+
+			foreach (Geo.Node current_node in nodes)
+			{
+				graph.AddEdge(new Edge(getVectorFromId(current_node.id, graph_nodes),
+					getVectorFromId(current_node.destination, graph_nodes), current_node.weight));
+			}
+
+			graph.SourceVertex = getVectorFromId(origin_id, graph_nodes);
+
+			bool reachable = graph.CalculateShortestPath();
+			List<Vector2D> path = graph.RetrieveShortestPath(getVectorFromId(destination_id, graph_nodes));
+
+			double cost = path[path.Count - 1].AggregateCost;
+
+			Console.WriteLine("---------------------");
+			Console.WriteLine("Ahora te daré todas las conexiones intermedias:");
+
+			foreach (Vector2D current_vector in path)
+			{
+				foreach (Geo.Place place in places)
+				{
+					if (place.id == current_vector.VertexID)
+					{
+						station_name = place.name;
+						Console.WriteLine(station_name);
+					}
+				}
+			}
+
+
+			string clientId = "CLIENT_ID";
+			string clientSecret = "CLIEND_SECRET";
+			string redirectUri = "REDIRECT_URI";
+			//SharpSquare sharpSquare = new SharpSquare(clientId, clientSecret);
+                    
+			/*
+			if (Request["code"] != null)
+			{
+				sharpSquare.GetAccessToken(redirectUri, Request["code"]);
+				// Here, you can do something
+			}
+			else
+			{
+				HyperLink.NavigateUrl = sharpSquare.GetAuthenticateUrl(redirectUri);
+			}  
+			 * */
+                    
+			Dictionary<string, string> parameters = new Dictionary<string, string>();
+			parameters.Add("ll", "44.3,37.2");
+			//List<Geo.Venue> venues = sharpSquare.SearchVenues(parameters);
+
+
+			/*
+			string sNew = "Plaza Galerias";
+			string sOld = "galerias";
+
+			try
+			{
+				Levenshtein l = new Levenshtein();
+
+				int counter = 0;
+				while (counter < 1)
+				{
+					DateTime startTime = DateTime.Now;
+					DateTime stopTime = DateTime.Now;
+					TimeSpan duration = stopTime - startTime;
+
+					startTime = DateTime.Now;
+
+					l.iLD(sNew, sOld);
+
+					stopTime = DateTime.Now;
+
+					duration = stopTime - startTime;
+					Console.WriteLine("New     :" + duration);
+					Console.WriteLine("----------------");
+
+					++counter;
+				}
+			}
+			catch (Exception e)
+			{
+				System.Console.WriteLine(e.ToString());
+			}
+			 */
+
+			Console.ReadKey();
+		}
+
+		public static Vector2D getVectorFromId(int id, List<Vector2D> nodes)
+		{
+			foreach (Vector2D current_node in nodes)
+			{
+				if (current_node.VertexID == id)
+				{
+					return current_node;
+				}
+			}
+
+			// TO DO: Correct this exception case
+			return new Vector2D(0, 0, false, 0);
+		}
+
+		/// <summary>
+		/// Get a list of venues, matching the name
+		/// TO DO:
+		/// - Fuzzy search when we get no results. (For example, if you search Four Square, you're not
+		/// going to find FourSquare
+		/// </summary>
+		/// <param name="search_string">Name of the venue to find</param>
+		/// <param name="venues">List of venues to </param>
+		/// <returns>List of venues where the search string is present</returns>
+		public static List<Geo.Venue> searchVenues (string search_string, List<Geo.Venue> venues)
+		{
+			List<Geo.Venue> found_venues = new List<Geo.Venue>();
+			search_string = replaceLatinCharacters(search_string.ToLower());
+
+			string[] search_words = search_string.Split(' ');
+
+			Dictionary<string, int> search_votes = new Dictionary<string, int>();
+
+			foreach (string current_word in search_words)
+			{
+				foreach (Geo.Venue current_venue in venues)
+				{
+					string venue_name = replaceLatinCharacters(current_venue.name).ToLower();
+					if (venue_name.Contains(current_word))
+					{
+						// Add votes each time you find a word
+						if (search_votes.ContainsKey(current_venue.id))
+						{
+							++search_votes[current_venue.id];
+						}
+						else
+						{
+							search_votes[current_venue.id] = 1;
+						}
+					}
+				}
+			}
+
+
+			foreach (KeyValuePair<string, int> vote in search_votes)
+			{
+				if (vote.Value > 0)
+				{
+					found_venues.Add(findVenueByID(vote.Key, venues));
+				}
+			}
+
+			return found_venues;
+		}
+
+		/// <summary>
+		/// From a list of Venues, get that venue matching with the id supplied
+		/// </summary>
+		/// <param name="id">String with the ID of the venue to search</param>
+		/// <param name="venues">List of venues to search</param>
+		/// <returns>Venue</returns>
+		public static Geo.Venue findVenueByID(string id, List<Geo.Venue> venues)
+		{
+			foreach (Geo.Venue current_venue in venues)
+			{
+				if (current_venue.id == id)
+				{
+					return current_venue;
+				}
+			}
+
+			return new Geo.Venue();
+		}
+
+		/// <summary>
+		/// Remove accents and tilde from the character
+		/// </summary>
+		/// <param name="string_to_replace"></param>
+		/// <returns>String with the latin characters removed</returns>
+		public static string replaceLatinCharacters(string string_to_replace)
+		{
+			string_to_replace = string_to_replace.Replace('á', 'a');
+			string_to_replace = string_to_replace.Replace('é', 'e');
+			string_to_replace = string_to_replace.Replace('í', 'i');
+			string_to_replace = string_to_replace.Replace('ó', 'o');
+			string_to_replace = string_to_replace.Replace('ú', 'u');
+			string_to_replace = string_to_replace.Replace('Á', 'A');
+			string_to_replace = string_to_replace.Replace('É', 'E');
+			string_to_replace = string_to_replace.Replace('Í', 'I');
+			string_to_replace = string_to_replace.Replace('Ó', 'O');
+			string_to_replace = string_to_replace.Replace('Ú', 'U');
+			string_to_replace = string_to_replace.Replace('Ú', 'U');
+			string_to_replace = string_to_replace.Replace('ü', 'u');
+			string_to_replace = string_to_replace.Replace('Ü', 'U');
+			string_to_replace = string_to_replace.Replace('ñ', 'n');
+			string_to_replace = string_to_replace.Replace('Ñ', 'N');
+
+			return string_to_replace;
+		}
+
+	}
+
+	public class Levenshtein
+	{
+		///*****************************
+		/// Compute Levenshtein distance 
+		/// Memory efficient version
+		///*****************************
+		public int iLD(String sRow, String sCol)
+		{
+			int RowLen = sRow.Length;  // length of sRow
+			int ColLen = sCol.Length;  // length of sCol
+			int RowIdx;                // iterates through sRow
+			int ColIdx;                // iterates through sCol
+			char Row_i;                // ith character of sRow
+			char Col_j;                // jth character of sCol
+			int cost;                   // cost
+
+			/// Test string length
+			if (Math.Max(sRow.Length, sCol.Length) > Math.Pow(2, 31))
+				throw (new Exception("\nMaximum string length in Levenshtein.iLD is " + Math.Pow(2, 31) + ".\nYours is " + Math.Max(sRow.Length, sCol.Length) + "."));
+
+			// Step 1
+
+			if (RowLen == 0)
+			{
+				return ColLen;
+			}
+
+			if (ColLen == 0)
+			{
+				return RowLen;
+			}
+
+			/// Create the two vectors
+			int[] v0 = new int[RowLen + 1];
+			int[] v1 = new int[RowLen + 1];
+			int[] vTmp;
+
+			/// Step 2
+			/// Initialize the first vector
+			for (RowIdx = 1; RowIdx <= RowLen; RowIdx++)
+			{
+				v0[RowIdx] = RowIdx;
+			}
+
+			// Step 3
+
+			/// Fore each column
+			for (ColIdx = 1; ColIdx <= ColLen; ColIdx++)
+			{
+				/// Set the 0'th element to the column number
+				v1[0] = ColIdx;
+
+				Col_j = sCol[ColIdx - 1];
+
+
+				// Step 4
+
+				/// Fore each row
+				for (RowIdx = 1; RowIdx <= RowLen; RowIdx++)
+				{
+					Row_i = sRow[RowIdx - 1];
+
+
+					// Step 5
+
+					if (Row_i == Col_j)
+					{
+						cost = 0;
+					}
+					else
+					{
+						cost = 1;
+					}
+
+					// Step 6
+
+					/// Find minimum
+					int m_min = v0[RowIdx] + 1;
+					int b = v1[RowIdx - 1] + 1;
+					int c = v0[RowIdx - 1] + cost;
+
+					if (b < m_min)
+					{
+						m_min = b;
+					}
+					if (c < m_min)
+					{
+						m_min = c;
+					}
+
+					v1[RowIdx] = m_min;
+				}
+
+				/// Swap the vectors
+				vTmp = v0;
+				v0 = v1;
+				v1 = vTmp;
+
+			}
+
+
+			// Step 7
+
+			/// Value between 0 - 100
+			/// 0==perfect match 100==totaly different
+			/// 
+			/// The vectors where swaped one last time at the end of the last loop,
+			/// that is why the result is now in v0 rather than in v1
+			System.Console.WriteLine("iDist=" + v0[RowLen]);
+			int max = System.Math.Max(RowLen, ColLen);
+			return ((100 * v0[RowLen]) / max);
+		}
+
+		///*****************************
+		/// Compute the min
+		///*****************************
+
+		private int Minimum(int a, int b, int c)
+		{
+			int mi = a;
+
+			if (b < mi)
+			{
+				mi = b;
+			}
+			if (c < mi)
+			{
+				mi = c;
+			}
+
+			return mi;
+		}
+
+		///*****************************
+		/// Compute Levenshtein distance         
+		///*****************************
+
+		public int LD(String sNew, String sOld)
+		{
+			int[,] matrix;              // matrix
+			int sNewLen = sNew.Length;  // length of sNew
+			int sOldLen = sOld.Length;  // length of sOld
+			int sNewIdx; // iterates through sNew
+			int sOldIdx; // iterates through sOld
+			char sNew_i; // ith character of sNew
+			char sOld_j; // jth character of sOld
+			int cost; // cost
+
+			/// Test string length
+			if (Math.Max(sNew.Length, sOld.Length) > Math.Pow(2, 31))
+				throw (new Exception("\nMaximum string length in Levenshtein.LD is " + Math.Pow(2, 31) + ".\nYours is " + Math.Max(sNew.Length, sOld.Length) + "."));
+
+			// Step 1
+
+			if (sNewLen == 0)
+			{
+				return sOldLen;
+			}
+
+			if (sOldLen == 0)
+			{
+				return sNewLen;
+			}
+
+			matrix = new int[sNewLen + 1, sOldLen + 1];
+
+			// Step 2
+
+			for (sNewIdx = 0; sNewIdx <= sNewLen; sNewIdx++)
+			{
+				matrix[sNewIdx, 0] = sNewIdx;
+			}
+
+			for (sOldIdx = 0; sOldIdx <= sOldLen; sOldIdx++)
+			{
+				matrix[0, sOldIdx] = sOldIdx;
+			}
+
+			// Step 3
+
+			for (sNewIdx = 1; sNewIdx <= sNewLen; sNewIdx++)
+			{
+				sNew_i = sNew[sNewIdx - 1];
+
+				// Step 4
+
+				for (sOldIdx = 1; sOldIdx <= sOldLen; sOldIdx++)
+				{
+					sOld_j = sOld[sOldIdx - 1];
+
+					// Step 5
+
+					if (sNew_i == sOld_j)
+					{
+						cost = 0;
+					}
+					else
+					{
+						cost = 1;
+					}
+
+					// Step 6
+
+					matrix[sNewIdx, sOldIdx] = Minimum(matrix[sNewIdx - 1, sOldIdx] + 1, matrix[sNewIdx, sOldIdx - 1] + 1, matrix[sNewIdx - 1, sOldIdx - 1] + cost);
+
+				}
+			}
+
+			// Step 7
+
+			/// Value between 0 - 100
+			/// 0==perfect match 100==totaly different
+			System.Console.WriteLine("Dist=" + matrix[sNewLen, sOldLen]);
+			int max = System.Math.Max(sNewLen, sOldLen);
+			return (100 * matrix[sNewLen, sOldLen]) / max;
+		}
+	}
+}
